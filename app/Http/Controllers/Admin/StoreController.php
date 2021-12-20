93d44e2ccdc4c37a0a86994834b0786fe6076b0c
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2021-12-19 23:40:56 -0300

    40. Iniciando Rotas Para Lojas

--- app/Http/Controllers/Admin/StoreController.php
+++ app/Http/Controllers/Admin/StoreController.php
@@ -4,8 +4,14 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Store;
 
 class StoreController extends Controller
 {
-    //
+    public function index()
+    {
+        $stores = Store::all();
+
+        return $stores;
+    }
 }
